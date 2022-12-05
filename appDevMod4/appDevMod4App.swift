7bf08a9ad4b0934ec449839a442f85a550476c3a
Max Huckstepp <mhuckstepp@strava.com>
2022-12-05 11:13:43 -0800

    Initial Commit

---
+++ appDevMod4/appDevMod4App.swift
@@ -0,0 +1,17 @@
+//
+//  appDevMod4App.swift
+//  appDevMod4
+//
+//  Created by Maxim Huckstepp on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct appDevMod4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
